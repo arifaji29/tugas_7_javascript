09ba02f0c8991e5fc857dedce88270aab4f8d556
Arif <arifajif@gmail.com>
2022-06-01 22:18:25 +0700

    tugas 7

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+var tinggi=[165,163,160,165,167,162,171,169,173,164]
+for (var i=0;i<tinggi.length;i++) {
+    console.log(tinggi[i])
+}
+
+var tinggi =[165,163,160,165,167,162,171,169,173,164]
+for (let x of tinggi) {
+    console.log(x);
+}
